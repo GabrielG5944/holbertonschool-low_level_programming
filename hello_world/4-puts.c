df96c6d00c61ed38f0d41e549b42c489c997586b
GabrielG5944 <4262@holbertonschool.com>
2022-06-06 20:56:48 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-/*
-puts - prints a string of characters
+/**
+* main - Entry point
+* Return: 0
 */
-
-int main(void){
+int main(void)
+{
 
 puts("Programming is like building a multilingual puzzle\n");
 return (0);
