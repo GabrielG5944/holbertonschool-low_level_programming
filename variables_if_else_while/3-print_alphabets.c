f72e238a2733cd6fdad087828bd1af4e6591e6c3
GabrielG5944 <4262@holbertonschool.com>
2022-06-07 17:29:17 +0000

    alph

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -6,10 +6,10 @@
 
 int main(void)
 {
-char ALPH[54] = "aabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char ALPH[53] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 int i = 0;
 
-while (i < 54)
+while (i < 53)
 {
 i++;
 putchar(ALPH[i]);
