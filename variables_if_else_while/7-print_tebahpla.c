60f146d920baef74c4a023f6944112c459c24d5d
GabrielG5944 <4262@holbertonschool.com>
2022-06-07 18:00:24 +0000

    i--

--- variables_if_else_while/7-print_tebahpla.c
+++ variables_if_else_while/7-print_tebahpla.c
@@ -1,10 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
-* main
+* main - prints alphabet except q and e
 * Return: 0
 */
 
 int main(void)
 {
-return (0):
+	int i;
+
+	for (i = 122; i < 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
 }
+
