79c537f252f0b2271f10dada4a63e8a7302ad0e8
GabrielG5944 <4262@holbertonschool.com>
2022-06-07 17:40:32 +0000

    qe

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 /**
-* main
+* main - prints alphabet except q and e
 * Return: 0
 */
 
 int main(void)
 {
-return (0):
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
