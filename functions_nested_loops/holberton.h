812f471888b0952ff9e3a4fc264314fee170898f
GabrielG5944 <4262@holbertonschool.com>
2022-06-11 15:40:24 +0000

    a

---
+++ functions_nested_loops/holberton.h
@@ -0,0 +1,12 @@
+#ifndef _holberton_h_
+#define _holberton_h_
+
+int _putchar(char);
+void print_alphabet(void);
+void printf_alphabet_x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int print_sign(int n);
+int _abs(int);
+
+#endif
